44ad310ab1bf8d2f69378f5313c956bb1d48df37
hss241 <honshin24@yahoo.co.jp>
2019-07-30 09:07:25 +0900

    first

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+	class main
+	{
+		public static void Main(string[] args)
+		{
+			Dengen d;
+
+			d = new AcAdapter();
+			int denatsu = d.kyuuden();
+			Console.WriteLine(denatsu + "Vで給電されています");
+			Console.ReadLine();
+		}
+	}
+
+	interface Dengen
+	{
+		int kyuuden();
+	}
+
+	class JapaneseConsent
+	{
+		public int power()
+		{
+			return 100;
+		}
+	}
+
+	class AcAdapter : Dengen
+	{
+		JapaneseConsent jc = new JapaneseConsent();
+		public int kyuuden()
+		{
+			return (int)(jc.power() * 0.16);
+		}
+	}
+}
